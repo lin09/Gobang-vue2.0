34d696f8eff52d2f4ac7fc6e41b3c77e7f7ec726
lin.zhenhui <315713565@163.com>
2018-11-25 22:10:08 +0800

    dist

--- src/router/index.js
+++ src/router/index.js
@@ -5,5 +5,6 @@ import routes from './routes'
 Vue.use(VueRouter)
 export default new VueRouter({
   mode: 'history',
+  base: window.location.hostname === 'lin09.github.io' ? '/Gobang-vue2.0/dist/index.html' : '/',
   routes
 })
