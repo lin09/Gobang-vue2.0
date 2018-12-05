5f6b5070708bc05ae24b2957c68fadec7fcad154
林振辉 <315713565@163.com>
2018-12-05 10:00:47 +0800

    更改路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
 
+// 个人仓信息
+const isGithub = window.location.hostname === 'lin09.github.io'
+
 Vue.use(VueRouter)
 export default new VueRouter({
-  mode: 'history',
-  base: window.location.hostname === 'lin09.github.io' ? '/Gobang-vue2.0/dist/index.html' : '/',
+  mode: isGithub ? 'hash' : 'history',
+  base: isGithub ? window.location.pathname : '/',
   routes
 })
