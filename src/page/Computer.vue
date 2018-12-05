881b9da928183be81179c92e314de0ed81779058
林振辉 <315713565@163.com>
2018-12-05 22:43:06 +0800

    添加页面

---
+++ src/page/Computer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="game">
+    <InfoTool />
+    <Checkerboard />
+  </div>
+</template>
+
+<script>
+import InfoTool from '../components/InfoTool'
+import Checkerboard from '../components/Checkerboard'
+
+export default {
+  name: 'game',
+  components: {
+    InfoTool,
+    Checkerboard
+  }
+}
+</script>
