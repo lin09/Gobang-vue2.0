c0357e7c87ce62a636f9197a86b24ec58e037ace
lin09 <315713565@163.com>
2020-12-01 12:19:57 +0800

    router base

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
 
+const pathnameRE = new RegExp('(/gobang-vue2.0/dist/)', 'i')
 // 个人仓信息
-const isGithub = window.location.hostname === 'lin09.github.io'
+const isGithub = pathnameRE.test(window.location.pathname)
 
 Vue.use(VueRouter)
 export default new VueRouter({
   mode: isGithub ? 'hash' : 'history',
-  base: isGithub ? window.location.pathname : '/',
+  base: isGithub ? window.location.pathname.replace(pathnameRE, '$1') : '/',
   routes
 })
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // 基本路径
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/gobang-vue2.0/dist/'
+    ? '/Gobang-vue2.0/dist/'
     : '/',
   // 生产环境是否生成 sourceMap 文件
   productionSourceMap: false,
