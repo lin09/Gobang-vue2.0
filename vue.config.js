b0b4788a70333a4038c1d9daa4088fcf227cdb6f
lin09 <315713565@163.com>
2020-12-01 11:58:54 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 
 module.exports = {
   // 基本路径
-  baseUrl: process.env.NODE_ENV === "production" ? '/Gobang-vue2.0/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/gobang-vue2.0/dist/'
+    : '/',
   // 生产环境是否生成 sourceMap 文件
   productionSourceMap: false,
   // 服务器端口号
